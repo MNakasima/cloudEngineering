eadebf36bb9fd47a6c53bd2cab0718d5d8c10583
Maruko <marco.k95@hotmail.com>
2023-10-07 22:49:13 -0300

    sqs para eventos pizza pronta

---
+++ sqs/handler.py
@@ -0,0 +1,15 @@
+from sqsHandler import SqsHandler
+import json
+
+def handler(event, context):
+    
+    sqs = SqsHandler("https://sqs.us-east-1.amazonaws.com/603867557236/espera-entrega")
+    
+    data = json.dumps(event)
+    python_data = json.loads(data)
+    
+    message = python_data['detail']['cliente'] + ' pedido: ' + str(python_data['detail']['pedido'])
+    
+    sqs.send(message)
+    
+    return event
